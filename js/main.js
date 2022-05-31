ab502867c23a353f1dd43089b2cb3814753bcf73
sinyng9928 <sinyng9928@naver.com>
2022-05-31 16:54:44 +0900

    core value 수정

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ let valueContent = $('.value_contents div');
 
 valueContent.hover(function(){
     $(this).addClass('active');
+    $(this).find('p').fadeIn();
 },
 function(){
     $(this).removeClass('active');
+    $(this).find('p').fadeOut();
 });
 
 /* Main Slide */
